3d2c5d84004b7997337b119ed2ac553107e21528
JJ20 <12671541+sinan1247@user.noreply.gitee.com>
2025-09-21 00:27:39 +0800

    优化代码

--- src/main/java/com/example/ocrtool/Main.java
+++ src/main/java/com/example/ocrtool/Main.java
@@ -4,14 +4,12 @@ import com.example.ocrtool.config.KeyMapping;
 import com.example.ocrtool.hotkey.GlobalHotkeyListener;
 import com.example.ocrtool.utils.PathDiagnostic;
 
-import java.net.URISyntaxException;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) {
         // 添加路径诊断
         PathDiagnostic.printPaths();
-        
         // 获取快捷键编码
         Set<Integer> hotkey = KeyMapping.loadHotkey();
         // 注册快捷键
